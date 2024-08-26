aafa8d8d1964320a24a041bc8f4bae0becb95f89
cokorie <chijioke.okorie@gmail.com>
2024-08-26 13:45:24 -0500

    add squares array

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ for (let i = 0; i < 225; i++) {
     grid.appendChild(square);
 };
 
+const squares = document.querySelectorAll();
+
 const alienInvaders = [
     0,1,2,3,4,5,6,7,8,9,
     15,16,17,18,19,20,21,22,23,24,
