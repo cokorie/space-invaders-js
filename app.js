e3e6462f92b4b3d1a0a530c417212ad269d61982
cokorie <chijioke.okorie@gmail.com>
2024-08-26 13:37:06 -0500

    add square to appendChild

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const grid = document.querySelector('.grid');
 
 for (let i = 0; i < 225; i++) {
     const square = document.createElement('div');
-    grid.appendChild();
+    grid.appendChild(square);
 };
