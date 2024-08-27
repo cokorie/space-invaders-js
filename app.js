af06528e0dbbe95d657c899df338ace50c15af4f
cokorie <chijioke.okorie@gmail.com>
2024-08-27 06:59:38 -0500

    add invadersId

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let currentShooterIndex = 202;
 let width = 15;
 let direction = 1;
 let goingRight = true;
+let invadersId;
 
 for (let i = 0; i < 225; i++) {
     const square = document.createElement('div');
