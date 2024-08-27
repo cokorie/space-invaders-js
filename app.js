66d56706139d94246b03132fe7a4a09b91508299
cokorie <chijioke.okorie@gmail.com>
2024-08-27 06:21:24 -0500

    define full rightEdge parameters

--- app.js
+++ app.js
@@ -42,5 +42,5 @@ document.addEventListener('keydown', moveShooter);
 
 function moveInvaders() {
     const leftEdge = alienInvaders[0] % width === 0;
-    const rightEdge = alienInvaders[alienInvaders.length - 1];
+    const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1;
 };
