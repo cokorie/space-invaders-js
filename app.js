e37d8eef441b67daef72e52be21d2f551ac872f9
cokorie <chijioke.okorie@gmail.com>
2024-08-27 06:48:49 -0500

    add invaders right false statement

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ function moveInvaders() {
         for (let i = 0; i < alienInvaders.length; i++) {
             alienInvaders[i] += width + 1;
             direction = -1;
-            
+            goingRight = false;
         };
     };
 
