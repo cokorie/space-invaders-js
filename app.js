46869b52789e0e2aa9927ca8b1eb3e351a433612
cokorie <chijioke.okorie@gmail.com>
2024-09-20 21:09:08 -0500

    update square currentLaserIndex syntax

--- app.js
+++ app.js
@@ -105,16 +105,16 @@ function shoot(e) {
     let laserId;
     let currentLaserIndex = currentShooterIndex;
     function moveLaser() {
-        squares.[currentLaserIndex].classList.remove('laser');
+        squares[currentLaserIndex].classList.remove('laser');
         currentLaserIndex -= width;
-        squares.[currentLaserIndex].classList.add('laser');
+        squares[currentLaserIndex].classList.add('laser');
         
-        if(squares.[currentLaserIndex].classList.contains('invader')) {
-            squares.[currentLaserIndex].classList.remove('laser');
-            squares.[currentLaserIndex].classList.remove('invader');
-            squares.[currentLaserIndex].classList.add('boom');
+        if(squares[currentLaserIndex].classList.contains('invader')) {
+            squares[currentLaserIndex].classList.remove('laser');
+            squares[currentLaserIndex].classList.remove('invader');
+            squares[currentLaserIndex].classList.add('boom');
 
-            setTimeout(()=> squares.[currentLaserIndex].classList.remove('boom'), 300);
+            setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300);
             clearInterval(laserId);
 
             const alienRemoved = alienInvaders.indexOf(currentLaserIndex);
