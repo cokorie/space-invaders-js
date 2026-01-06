a3d98861ed874193605658e22329d689ae8ad488
cokorie <chijioke.okorie@gmail.com>
2026-01-06 10:11:25 -0600

    add enemy imaging

--- Enemy.js
+++ Enemy.js
@@ -2,5 +2,8 @@ export default class Enemy {
     constructor(x,y,imageNumber) {
         this.x = x;
         this.y = y;
+
+        this.image = new Image();
+        this.image.src = `images/enemy${imageNumber}`;
     }
 }
