44fb2b52ae34754c7d609ab16f5a0e2e9bbe34ba
cokorie <chijioke.okorie@gmail.com>
2026-01-06 09:53:41 -0600

    add enemyMap

--- EnemyController.js
+++ EnemyController.js
@@ -1,6 +1,15 @@
 export default class EnemyController {
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    ];
+
     constructor(canvas) {
         this.canvas = canvas;
     }
-    draw(ctx) {};
+    draw(ctx) { };
 }
