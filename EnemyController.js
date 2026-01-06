ae6249458200a5e9291418f76dff7a396874245e
cokorie <chijioke.okorie@gmail.com>
2026-01-06 12:40:30 -0600

    add newDirection return false

--- EnemyController.js
+++ EnemyController.js
@@ -57,6 +57,7 @@ export default class EnemyController {
             this.currentDirection = newDirection;
             return true;
         }
+        return false;
     }
 
     drawEnemies(ctx) {
