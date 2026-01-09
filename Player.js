063b7de5aec15296faf35a9dd22cd0009c9abe98
cokorie <chijioke.okorie@gmail.com>
2026-01-09 17:07:29 -0600

    add shoot gap

--- Player.js
+++ Player.js
@@ -20,7 +20,7 @@ export default class Player {
 
     draw(ctx){
         if (this.shootPressed) {
-            this.bulletController.shoot(this.x + this.width / 2, this.y, 4);
+            this.bulletController.shoot(this.x + this.width / 2, this.y, 4, 10);
         }
         this.move();
         this.collideWithWalls();
