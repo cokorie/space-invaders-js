d6a23c0e65e8eb4067b9ea278bcaaa791a6a5aa6
cokorie <chijioke.okorie@gmail.com>
2026-01-09 08:54:34 -0600

    add syntax to drawImage and import Player to main js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import EnemyController from "./EnemyController.js";
+import Player from "./Player.js";
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
@@ -10,6 +11,7 @@ const background = new Image();
 background.src = "images/space.png";
 
 const enemyController = new EnemyController(canvas);
+
 
 function game () {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
