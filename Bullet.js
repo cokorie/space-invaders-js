b86167ecfea42f974d98f99af534ee11aee0c467
cokorie <chijioke.okorie@gmail.com>
2026-01-09 22:34:35 -0600

    add collideWith if statement conditionals

--- Bullet.js
+++ Bullet.js
@@ -18,7 +18,7 @@ export default class Bullet {
     }
 
     collideWith(sprite) {
-        if () {
+        if (this.x + this.width > sprite.x && this.x < sprite.x + sprite.width && this.y + this.height > sprite.y && this.y < sprite.y + sprite.height) {
             
         }
     }
