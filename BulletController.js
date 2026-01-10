907902886a5975121c7631befca0ae157e9724b1
cokorie <chijioke.okorie@gmail.com>
2026-01-09 20:16:30 -0600

    add constructor bulletColor

--- BulletController.js
+++ BulletController.js
@@ -3,6 +3,7 @@ export default class BulletController () {
     constructor(canvas, maxBulletsAtATime, bulletColor, soundEnabled) {
         this.canvas = canvas;
         this.maxBulletsAtATime = maxBulletsAtATime;
+        this.bulletColor = bulletColor;
     }
 
     shoot() {
