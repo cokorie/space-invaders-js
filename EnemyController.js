34dc9d2eee33659334ae146e97952f94463d26a0
cokorie <chijioke.okorie@gmail.com>
2026-01-09 22:21:23 -0600

    add enemy collision sound splice

--- EnemyController.js
+++ EnemyController.js
@@ -44,7 +44,8 @@ export default class EnemyController {
         this.enemyRows.forEach((enemyRow)) => {
             this.enemyRow.forEach((enemy, enemyIndex)) => {
                 if (this.playerBulletController.collideWith(enemy)) {
-                    
+                    // to play a sound when detected
+                    enemyRow.splice(enemyIndex, 1);
                 }
             }
         }
