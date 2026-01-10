95393368a1dc1c199229f4e2347a5b894e768af1
cokorie <chijioke.okorie@gmail.com>
2026-01-09 22:13:37 -0600

    add fireBullet to draw function

--- EnemyController.js
+++ EnemyController.js
@@ -36,6 +36,7 @@ export default class EnemyController {
         this.updateVelocityAndDirection();
         this.drawEnemies(ctx);
         this.resetMoveDownTimer();
+        this.fireBullet();
     }
 
     fireBullet() {
