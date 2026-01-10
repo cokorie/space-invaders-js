29de995c50650f6a091e504e2e11ca177972c31e
cokorie <chijioke.okorie@gmail.com>
2026-01-09 22:35:46 -0600

    add collideWith else false return

--- Bullet.js
+++ Bullet.js
@@ -21,7 +21,7 @@ export default class Bullet {
         if (this.x + this.width > sprite.x && this.x < sprite.x + sprite.width && this.y + this.height > sprite.y && this.y < sprite.y + sprite.height) {
             return true;
         } else {
-            
+            return false;
         }
     }
 }
