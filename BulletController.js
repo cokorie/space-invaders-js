5c10064d5019ae8508f3b502ff241a66bd435684
cokorie <chijioke.okorie@gmail.com>
2026-01-09 20:30:57 -0600

    play shoot sound

--- BulletController.js
+++ BulletController.js
@@ -20,6 +20,7 @@ export default class BulletController () {
             this.bullets.push(bullet);
             if (this.soundEnabled) {
                 this.shootSound.currentTime = 0;
+                this.shootSound.play();
             }
         }
     }
