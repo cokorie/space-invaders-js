615ddea6833116cb7e45f76f629538795041badd
cokorie <chijioke.okorie@gmail.com>
2026-01-09 21:58:03 -0600

    pass enemyBulletController in enenyController

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ background.src = "images/space.png";
 
 const playerBulletController = new BulletController(canvas, 10, "red", true);
 const enemyBulletController = new BulletController(canvas, 4, "white", false);
-const enemyController = new EnemyController(canvas);
+const enemyController = new EnemyController(canvas, enemyBulletController);
 
 const player = new Player(canvas, 3, playerBulletController);
 
