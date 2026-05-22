b81b531df044cb1fbdebd8d5dd14ab37ec5a5404
cokorie <chijioke.okorie@gmail.com>
2026-05-22 12:10:37 -0500

    add orig game player width

--- Player.js
+++ Player.js
@@ -10,7 +10,7 @@ export default class Player {
 
         this.x = this.canvas.width / 2;
         this.y = this.canvas.height - 75;
-        this.width = 100;
+        this.width = 50;
         this.height = 48;
         this.image = new Image();
         this.image.src = "images/player.png";
